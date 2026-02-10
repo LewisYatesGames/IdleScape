90f2f805f30092698b936fc82e7b10ff18af5437
Lewis <lewisyatesgames@gmail.com>
2026-02-10 16:38:06 +0000

    Fix formatting in Player

--- src/main/java/com/lyg/idlescape/server/player/Player.java
+++ src/main/java/com/lyg/idlescape/server/player/Player.java
@@ -11,7 +11,6 @@ import java.util.Map;
 public class Player {
     private final Map<SkillType, SkillProgress> skills = new EnumMap<>(SkillType.class);
 
-    private SkillProgress getSkill(SkillType skill){
     private SkillAction skillAction;
 
     public void update() {
@@ -43,19 +42,19 @@ public class Player {
         this.skillAction.start();
     }
 
+    private SkillProgress getSkill(SkillType skill) {
         return skills.computeIfAbsent(skill, _ -> new SkillProgress(0));
     }
 
-    public int getSkillExp(SkillType skill){
+    public int getSkillExp(SkillType skill) {
         return getSkill(skill).getExperience();
     }
 
-    public int getSkillLevel(SkillType skill){
+    public int getSkillLevel(SkillType skill) {
         return getSkill(skill).getSkillLevel();
     }
 
-    public void addSkillExp(SkillType skill, int exp){
+    public void addSkillExp(SkillType skill, int exp) {
         getSkill(skill).addExperience(exp);
     }
-
 }
